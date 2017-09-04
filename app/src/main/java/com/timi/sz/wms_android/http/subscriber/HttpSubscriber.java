789c0bac69769396b2ed49a431d55cd7d241be9d
jzk <893721871@qq.com>
2017-09-04 13:41:46 +0800

    500  返回的修改

--- app/src/main/java/com/timi/sz/wms_android/http/subscriber/HttpSubscriber.java
+++ app/src/main/java/com/timi/sz/wms_android/http/subscriber/HttpSubscriber.java
@@ -77,32 +77,36 @@ public class HttpSubscriber<T> implements Observer<T> {
                 }
             }
         } else if (e instanceof HttpException) {//服务器 错误 连接超时
-            String messageStr="";//提示信息
+            /**
+             * 当 webapi 返回失败的  500 时 进行解析一场
+             * 对异常返回数据 进行捕获 拿出message details 信息
+             */
+            String messageStr = "";//提示信息
             ResponseBody responseBody = ((HttpException) e).response().errorBody();
             try {
                 assert responseBody != null;
                 HttpResultBean httpResultBean = new Gson().fromJson(responseBody.string(), HttpResultBean.class);
-                if(null==httpResultBean){//为空则返回
+                if (null == httpResultBean) {//为空则返回
                     mOnResultListener.onError(SERVER_TIMEOUT_EXCEPTION);
                     return;
                 }
-                if(!TextUtils.isEmpty(httpResultBean.getError().getMessage())){
-                    messageStr=httpResultBean.getError().getMessage();
+                if (!TextUtils.isEmpty(httpResultBean.getError().getMessage())) {
+                    messageStr = httpResultBean.getError().getMessage();
                 }
-                if(!TextUtils.isEmpty(httpResultBean.getError().getDetails())){
-                    messageStr=httpResultBean.getError().getDetails();
+                if (!TextUtils.isEmpty(httpResultBean.getError().getDetails())) {
+                    messageStr = httpResultBean.getError().getDetails();
                 }
                 ToastUtils.showShort(messageStr);
                 mOnResultListener.onError(messageStr);
-                LogUitls.e("打印输出错误代码httpResultBean---->",httpResultBean.toString());
+                LogUitls.e("打印输出错误代码httpResultBean---->", httpResultBean.toString());
                 return;
             } catch (Exception e1) {
                 mOnResultListener.onError(SERVER_TIMEOUT_EXCEPTION);
             }
 
-        } else if(e instanceof UnknownHostException){// 测试到时再没网的情况下
+        } else if (e instanceof UnknownHostException) {// 测试到时再没网的情况下
             mOnResultListener.onError(CONNECT_EXCEPTION);
-        }else {//
+        } else {//
             /**
              * 弹出提示 所有的后台返回的提示
              */
